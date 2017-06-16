9211f6d401827eb4f01d5fff4bc47a74e343f367
Dominic Alie <dominic@georadix.com>
2017-06-16 19:40:45 -0400

    Scale map and add draggable token

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -10,36 +10,48 @@ import { Component, OnInit } from '@angular/core';
 export class GameComponent implements OnInit {
     private game: Phaser.Game;
     private inputs: Phaser.CursorKeys;
-
+    private player: Phaser.Sprite;
+    private mapSize: Phaser.Point;
+    private mapScale = 10;
     constructor() { }
 
     ngOnInit() {
+        
         this.game = new Phaser.Game('100', '100', Phaser.AUTO, 'game-canvas',
             { preload: this.preload, create: this.create, update: this.update });
     }
 
     private preload(): void {
         this.game.load.image('map', 'assets/keep.jpg');
+        this.game.load.image('token', 'assets/token.png');
     }
     private create(): void {
-        var mapImage = this.game.cache.getImage('map');
+        this.mapSize = new Phaser.Point(150, 150);
 
         // we need to add margin to the world, so the camera can move
-        var margin = 500;
+        var margin = this.mapSize.x / 2;
         // and set the world's bounds according to the given margin
         var x = -margin;
         var y = -margin;
-        var w = mapImage.width + margin * 2;
-        var h = mapImage.height + margin * 2;
+        var w = this.mapSize.x + margin * 2;
+        var h = this.mapSize.y + margin * 2;
         // it's not necessary to increase height, we do it to keep uniformity
         this.game.world.setBounds(x, y, w, h);
-
+        this.game.world.scale.setTo(10, 10);
         // we make sure camera is at position (0,0)
         this.game.world.camera.position.set(0, 0);
 
         // include some props on the scene
         var mapSprite = this.game.add.sprite(0, 0, 'map');
         mapSprite.inputEnabled = true;
+        mapSprite.width = 150;
+        mapSprite.height = 150;
+
+        this.player = this.game.add.sprite(this.mapSize.x / 2, this.mapSize.y / 2, 'token');
+        this.player.width = 5;
+        this.player.height = 5;
+        this.player.inputEnabled = true;
+        this.player.input.enableDrag(true);
 
         var dragging = false;
         var dragStart;
